fa1aac989a5dd1bcc1496857d2e2e6117a5b61b6
MRS <pile.o.junk@gmail.com>
2026-09-21 19:11:56 -0400

    Remove retired constant LR draft state

--- tool/js/training_workspace_state.js
+++ tool/js/training_workspace_state.js
@@ -47,7 +47,6 @@ var trainingWorkspaceState = {
   reviewInitializers: [],
   reviewInitializerExportId: '',
   reviewInitializerCustomPath: '',
-  reviewForceConstantLr: '',
   runConfigDraft: null,
   runConfigStage: '',
   runConfigTemplateSignature: '',

c1ae2fdd71b22adf89be60016bc5a2d68b9ac883
MRS <pile.o.junk@gmail.com>
2026-09-22 21:57:54 -0400

    Surface Storyboard generation in global workload status

--- tool/js/workspace_shell.js
+++ tool/js/workspace_shell.js
@@ -246,7 +246,8 @@ function formatShellDiskSpace(value) {
 
 var shellWorkloadState = {
   trainingActive: false,
-  testingActive: false
+  testingActive: false,
+  generatingActive: false
 };
 
 function getShellWorkloadStatus() {
@@ -255,6 +256,9 @@ function getShellWorkloadStatus() {
   }
   if (shellWorkloadState.testingActive) {
     return { key: 'testing', label: 'Testing' };
+  }
+  if (shellWorkloadState.generatingActive) {
+    return { key: 'generating', label: 'Generating' };
   }
   return { key: 'idle', label: 'Idle' };
 }
@@ -266,6 +270,11 @@ function setShellTrainingActive(active) {
 
 function setShellTestingActive(active) {
   shellWorkloadState.testingActive = !!active;
+  renderShellSystemStatus();
+}
+
+function setShellGeneratingActive(active) {
+  shellWorkloadState.generatingActive = !!active;
   renderShellSystemStatus();
 }
 
